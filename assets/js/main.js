0895b39f7a4a8191849cff3d4d83d5e0cb906c06
CelebRanking <privat.acc.for.insta@gmail.com>
2022-11-20 18:24:18 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -434,7 +434,6 @@ $(document).ready(function () {
             $('#rank-r' + i).html(`Rank ${myJson[celeb]['rank']}, ELO ${myJson[celeb]['rat']} ${myJson[celeb]['rchange'] === "-" ? "" : myJson[celeb]['rchange']}`);
             $('#img-r' + i).attr('src', `assets/img/${celeb}.jpg`);
             $('#ahref-r' + i).attr('href', `https://celebranking.github.io/details?name=${celeb}`)
-            $('#job').html(myJson[celeb]['job'])
 
             i++;
 
