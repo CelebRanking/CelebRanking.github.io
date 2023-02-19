bff2cc9b25e5892aad6c1e920c14a80335dda35b
CelebRanking <privat.acc.for.insta@gmail.com>
2023-02-19 13:33:25 +0100

    Update ranking-test2.js

--- ranking-test2.js
+++ ranking-test2.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $.getJSON("data.json", function(data) {
+  $.getJSON("ratings.json", function(data) {
     $.each(data, function(index, value) {
       $("#myTable tbody").append("<tr><td>" + value.name + "</td><td>" + value.ratingFace + "</td><td>" + value.ratingTits + "</td><td>" + value.ratingAss + "</td><td>" + value.ratingOverall + "</td></tr>");
     });
