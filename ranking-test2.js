91dfb4815cc54605a1694533cb17095532a09978
CelebRanking <privat.acc.for.insta@gmail.com>
2023-02-19 13:44:52 +0100

    highlight high value

--- ranking-test2.js
+++ ranking-test2.js
@@ -1,9 +1,30 @@
 $(document).ready(function() {
-  $.getJSON("ratings.json", function(data) {
-    $.each(data, function(index, value) {
-      $("#myTable tbody").append("<tr><td>" + value.name + "</td><td>" + value.ratingFace + "</td><td>" + value.ratingTits + "</td><td>" + value.ratingAss + "</td><td>" + value.ratingOverall + "</td></tr>");
+  // Load JSON data
+  $.getJSON('ratings.json', function(data) {
+    // Populate table with data
+    $('#myTable').DataTable({
+      data: data,
+      columns: [
+        { data: 'name' },
+        { data: 'rating Face' },
+        { data: 'rating Tits' },
+        { data: 'rating Ass' },
+        { data: 'rating Overall' }
+      ]
     });
 
-    $('#myTable').DataTable();
+    // Highlight the highest value in each column
+    $('table tr').each(function() {
+      $(this).find('td').not(':first-child').each(function(index) {
+        var columnData = [];
+        $('table tr td:nth-child(' + (index + 2) + ')').each(function() {
+          columnData.push(parseFloat($(this).text()));
+        });
+        var max = Math.max.apply(null, columnData);
+        if (parseFloat($(this).text()) == max) {
+          $(this).addClass('highlight');
+        }
+      });
+    });
   });
 });
