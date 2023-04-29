607fcf5a47ecf311bbdebe334d6e6993a9ff863e
CelebRanking <privat.acc.for.insta@gmail.com>
2023-04-30 01:21:28 +0200

    updated

--- interactive.js
+++ interactive.js
@@ -1,4 +1,42 @@
-function showImage(src) {
-    var ausgewaehlt = document.getElementById("ausgewaehlt");
-    ausgewaehlt.src = src;
-}
+const situations = [
+	{
+	  image: "assets/img/Hailee Steinfeld.jpg",
+	  title: "Situation 1",
+	  description: "Dies ist eine Beschreibung von Situation 1.",
+	  options: [
+		{
+		  image: "assets/img/Hailee Steinfeld.jpg",
+		  title: "Option 1 für Situation 1",
+		  description: "Dies ist eine Beschreibung von Option 1 für Situation 1."
+		},
+		{
+		  image: "assets/img/Zendaya.jpg",
+		  title: "Option 2 für Situation 1",
+		  description: "Dies ist eine Beschreibung von Option 2 für Situation 1."
+		},
+		{
+		  image: "assets/img/Gal Gadot.jpg",
+		  title: "Option 3 für Situation 1",
+		  description: "Dies ist eine Beschreibung von Option 3 für Situation 1."
+		}
+	  ]
+	},
+	{
+	  image: "assets/img/Zendaya.jpg",
+	  title: "Situation 2",
+	  description: "Dies ist eine Beschreibung von Situation 2.",
+	  options: [
+		{
+		  image: "assets/img/Hailee Steinfeld.jpg",
+		  title: "Option 1 für Situation 2",
+		  description: "Dies ist eine Beschreibung von Option 1 für Situation 2."
+		},
+		{
+		  image: "assets/img/Zendaya.jpg",
+		  title: "Option 2 für Situation",
+		  description: "Dies ist eine Beschreibung von Option 2 für Situation 2."
+		}
+	  ]
+	},
+]
+  
