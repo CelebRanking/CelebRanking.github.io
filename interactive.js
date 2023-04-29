f480b125bcf7436124320cb1aeb9d83c4217532a
CelebRanking <privat.acc.for.insta@gmail.com>
2023-04-30 01:45:31 +0200

    updated

--- interactive.js
+++ interactive.js
@@ -1,11 +1,19 @@
 const situationsContainer = document.getElementById('situations');
 const situationsContainer2 = document.getElementById('situations2');
 
-situationsContainer.addEventListener('click', function() {
-  situationsContainer.classList.add('hidden');
-  situationsContainer2.classList.remove('hidden');
+situationsContainer.addEventListener('click', function(event) {
+  const clickedImage = event.target;
+  const situationNumber = clickedImage.getAttribute('data-situation');
+  if (situationNumber) {
+    situationsContainer.classList.add('hidden');
+    situationsContainer2.classList.remove('hidden');
+  }
 });
 
-situationsContainer2.addEventListener('click', function() {
-  // handle clicks on situations in the second container
+situationsContainer2.addEventListener('click', function(event) {
+  const clickedImage = event.target;
+  const situationNumber = clickedImage.getAttribute('data-situation');
+  if (situationNumber) {
+    // handle clicks on situations in the second container
+  }
 });
