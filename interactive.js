5cdb2418fdd53cbc025e7368f0c32c7183f99bff
CelebRanking <privat.acc.for.insta@gmail.com>
2023-04-30 01:12:53 +0200

    updated

--- interactive.js
+++ interactive.js
@@ -1,13 +1,4 @@
-function showImage(src, option) {
-	var ausgewaehlt = document.getElementById("ausgewaehlt");
-	ausgewaehlt.src = src;
-	
-	// Entferne die 'ausgewaehlt'-Klasse von allen Optionen
-	var options = document.getElementsByClassName("option");
-	for (var i = 0; i < options.length; i++) {
-		options[i].classList.remove("ausgewaehlt");
-	}
-	
-	// Füge die 'ausgewaehlt'-Klasse zur ausgewählten Option hinzu
-	option.classList.add("ausgewaehlt");
+function showImage(src) {
+    var ausgewaehlt = document.getElementById("ausgewaehlt");
+    ausgewaehlt.src = src;
 }
