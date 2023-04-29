70a1bc342b1f9dc416b98872038b166996b770db
CelebRanking <privat.acc.for.insta@gmail.com>
2023-04-30 01:50:23 +0200

    updated

--- interactive.js
+++ interactive.js
@@ -1,19 +1,9 @@
-const situationsContainer = document.getElementById('situations');
-const situationsContainer2 = document.getElementById('situations2');
-
-situationsContainer.addEventListener('click', function(event) {
-  const clickedImage = event.target;
-  const situationNumber = clickedImage.getAttribute('data-situation');
-  if (situationNumber) {
-    situationsContainer.classList.add('hidden');
-    situationsContainer2.classList.remove('hidden');
-  }
-});
-
-situationsContainer2.addEventListener('click', function(event) {
-  const clickedImage = event.target;
-  const situationNumber = clickedImage.getAttribute('data-situation');
-  if (situationNumber) {
-    // handle clicks on situations in the second container
-  }
+const situations = document.querySelectorAll('.situation');
+situations.forEach(situation => {
+  situation.addEventListener('click', () => {
+	// Entferne die Klasse 'selected' von allen Situationen
+	situations.forEach(s => s.classList.remove('selected'));
+	// Füge die Klasse 'selected' zur angeklickten Situation hinzu
+	situation.classList.add('selected');
+  });
 });
