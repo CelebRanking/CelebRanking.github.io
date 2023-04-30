482f2d6602df762f912604a86cdcca097ab0abd5
CelebRanking <privat.acc.for.insta@gmail.com>
2023-04-30 10:24:21 +0200

    Update interactive.js

--- interactive.js
+++ interactive.js
@@ -1,43 +1,39 @@
-const images1 = [
+const images = [
+  [
   "assets/img/Alexa Bliss.jpg",
   "assets/img/Bayley.jpg",
   "assets/img/Carmella.jpg",
+  ],
+  [
   "assets/img/Victoria Justice.jpg",
   "assets/img/Shakira.jpg",
   "assets/img/Zendaya.jpg"
+  ]
 ];
 
-const images2 = [
-  "assets/img/Victoria Justice.jpg",
-  "assets/img/Shakira.jpg",
-  "assets/img/Zendaya.jpg"
-];
-
-const selectImage = (id, selection) => {
+const selectImage = (id, selectionDivId, imageArrayIndex) => {
   const selectedImage = document.getElementById(id);
   selectedImage.classList.add("selected");
   selectedImage.onclick = null;
 
-  const selectionDiv = document.getElementById(selection);
+  const selectionDiv = document.getElementById(selectionDivId);
   selectionDiv.innerHTML = "";
 
-  const images = (selection === "selection1") ? images2 : images1;
-  
-  images.forEach((image, index) => {
+  images[imageArrayIndex].forEach((image, index) => {
     const imageDiv = document.createElement("div");
     imageDiv.classList.add("col-md-4");
     const img = document.createElement("img");
     img.src = image;
     img.classList.add("img-fluid", "rounded");
-    img.onclick = () => selectImage(`image${index+4}`, selection === "selection1" ? "selection2" : "selection3");
+    img.onclick = () => selectImage(`image${index+4+imageArrayIndex*3+1}`, `selection${imageArrayIndex+1}`, imageArrayIndex+1);
     imageDiv.appendChild(img);
     const p = document.createElement("p");
-    p.textContent = `Bild ${index+4}`;
+    p.textContent = `Bild ${index+4+imageArrayIndex*3+1}`;
     imageDiv.appendChild(p);
     selectionDiv.appendChild(imageDiv);
   });
 };
 
-document.getElementById("image1").onclick = () => selectImage("image1", "selection1");
-document.getElementById("image2").onclick = () => selectImage("image2", "selection1");
-document.getElementById("image3").onclick = () => selectImage("image3", "selection1");
+document.getElementById("image1").onclick = () => selectImage("image1");
+document.getElementById("image2").onclick = () => selectImage("image2");
+document.getElementById("image3").onclick = () => selectImage("image3");
