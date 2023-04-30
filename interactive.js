235800c3ae7b69863805a6f422db980ef4850a06
CelebRanking <privat.acc.for.insta@gmail.com>
2023-04-30 09:28:45 +0200

    updated

--- interactive.js
+++ interactive.js
@@ -1,19 +1,32 @@
-const images = document.querySelectorAll(".image");
+const images = [
+  "assets/img/Alexa Bliss.jpg",
+  "assets/img/Bayley.jpg",
+  "assets/img/Carmella.jpg"
+];
 
-images.forEach((image) => {
-  image.addEventListener("click", () => {
-    // remove the 'selected' class from all images
-    images.forEach((img) => {
-      img.classList.remove("selected");
-    });
-    // add the 'selected' class to the clicked image
-    image.classList.add("selected");
-    // generate three new images to choose from
-    const gallery = document.querySelector(".gallery");
-    gallery.innerHTML = `
-      <img class="image" src="assets/img/Bayley.jpg">
-      <img class="image" src="assets/img/Carmella.jpg">
-      <img class="image" src="assets/img/Alexa Bliss.jpg">
-    `;
+const selectImage = (id) => {
+  const selectedImage = document.getElementById(id);
+  selectedImage.classList.add("selected");
+  selectedImage.onclick = null;
+
+  const selectionDiv = document.getElementById("selection");
+  selectionDiv.innerHTML = "";
+
+  images.forEach((image, index) => {
+    const imageDiv = document.createElement("div");
+    imageDiv.classList.add("col-md-4");
+    const img = document.createElement("img");
+    img.src = image;
+    img.classList.add("img-fluid", "rounded");
+    img.onclick = () => selectImage(`image${index+4}`);
+    imageDiv.appendChild(img);
+    const p = document.createElement("p");
+    p.textContent = `Bild ${index+4}`;
+    imageDiv.appendChild(p);
+    selectionDiv.appendChild(imageDiv);
   });
-});
+};
+
+document.getElementById("image1").onclick = () => selectImage("image1");
+document.getElementById("image2").onclick = () => selectImage("image2");
+document.getElementById("image3").onclick = () => selectImage("image3");
