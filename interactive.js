1ecf7506ed9c82b402cd0e4bc8fe9071ca934952
CelebRanking <privat.acc.for.insta@gmail.com>
2023-04-30 10:10:24 +0200

    test

--- interactive.js
+++ interactive.js
@@ -1,4 +1,4 @@
-const images = [
+const images1 = [
   "assets/img/Alexa Bliss.jpg",
   "assets/img/Bayley.jpg",
   "assets/img/Carmella.jpg",
@@ -7,21 +7,29 @@ const images = [
   "assets/img/Zendaya.jpg"
 ];
 
-const selectImage = (id) => {
+const images2 = [
+  "assets/img/Victoria Justice.jpg",
+  "assets/img/Shakira.jpg",
+  "assets/img/Zendaya.jpg"
+];
+
+const selectImage = (id, selection) => {
   const selectedImage = document.getElementById(id);
   selectedImage.classList.add("selected");
   selectedImage.onclick = null;
 
-  const selectionDiv = document.getElementById("selection");
+  const selectionDiv = document.getElementById(selection);
   selectionDiv.innerHTML = "";
 
+  const images = (selection === "selection1") ? images2 : images1;
+  
   images.forEach((image, index) => {
     const imageDiv = document.createElement("div");
     imageDiv.classList.add("col-md-4");
     const img = document.createElement("img");
     img.src = image;
     img.classList.add("img-fluid", "rounded");
-    img.onclick = () => selectImage(`image${index+4}`);
+    img.onclick = () => selectImage(`image${index+4}`, selection === "selection1" ? "selection2" : "selection3");
     imageDiv.appendChild(img);
     const p = document.createElement("p");
     p.textContent = `Bild ${index+4}`;
@@ -33,6 +41,3 @@ const selectImage = (id) => {
 document.getElementById("image1").onclick = () => selectImage("image1");
 document.getElementById("image2").onclick = () => selectImage("image2");
 document.getElementById("image3").onclick = () => selectImage("image3");
-document.getElementById("image4").onclick = () => selectImage("image4");
-document.getElementById("image5").onclick = () => selectImage("image5");
-document.getElementById("image6").onclick = () => selectImage("image6");
