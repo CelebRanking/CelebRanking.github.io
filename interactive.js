1dafc8dafc78d4ffd48bf04c41a3e9a6c5fb6350
CelebRanking <privat.acc.for.insta@gmail.com>
2023-04-30 18:32:21 +0200

    updated

--- interactive.js
+++ interactive.js
@@ -1,39 +1,27 @@
-const images = [
-  [
-  "assets/img/Alexa Bliss.jpg",
-  "assets/img/Bayley.jpg",
-  "assets/img/Carmella.jpg",
-  ],
-  [
-  "assets/img/Victoria Justice.jpg",
-  "assets/img/Shakira.jpg",
-  "assets/img/Zendaya.jpg"
-  ]
-];
+let budget = 30;
 
-const selectImage = (id, selectionDivId, imageArrayIndex) => {
+const updateBudget = () => {
+  document.getElementById("budget").textContent = budget;
+};
+
+const selectImage = (id) => {
   const selectedImage = document.getElementById(id);
-  selectedImage.classList.add("selected");
-  selectedImage.onclick = null;
-
-  const selectionDiv = document.getElementById(selectionDivId);
-  selectionDiv.innerHTML = "";
-
-  images[imageArrayIndex].forEach((image, index) => {
-    const imageDiv = document.createElement("div");
-    imageDiv.classList.add("col-md-4");
-    const img = document.createElement("img");
-    img.src = image;
-    img.classList.add("img-fluid", "rounded");
-    img.onclick = () => selectImage(`image${index+4+imageArrayIndex*3+1}`, `selection${imageArrayIndex+1}`, imageArrayIndex+1);
-    imageDiv.appendChild(img);
-    const p = document.createElement("p");
-    p.textContent = `Bild ${index+4+imageArrayIndex*3+1}`;
-    imageDiv.appendChild(p);
-    selectionDiv.appendChild(imageDiv);
-  });
+  const price = parseInt(selectedImage.dataset.price);
+  
+  if (selectedImage.classList.contains("selected")) {
+    budget += price;
+    selectedImage.classList.remove("selected");
+  } else if (budget >= price) {
+    budget -= price;
+    selectedImage.classList.add("selected");
+  }
+  
+  updateBudget();
 };
 
 document.getElementById("image1").onclick = () => selectImage("image1");
 document.getElementById("image2").onclick = () => selectImage("image2");
 document.getElementById("image3").onclick = () => selectImage("image3");
+document.getElementById("image4").onclick = () => selectImage("image4");
+document.getElementById("image5").onclick = () => selectImage("image5");
+document.getElementById("image6").onclick = () => selectImage("image6");
