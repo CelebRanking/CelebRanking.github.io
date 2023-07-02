22ae599f38fa1a43db19b5ff108e6e09ea1af828
CelebRanking <privat.acc.for.insta@gmail.com>
2023-07-02 10:25:29 +0200

    small changes

--- interactive/harembuilder3.js
+++ interactive/harembuilder3.js
@@ -35,31 +35,32 @@ const selectImage = (id) => {
   updateselectedcelebs();
   };
 
-//5$
+//6 Points
 document.getElementById("Alexandra Daddario").onclick = () => selectImage("Alexandra Daddario");
 document.getElementById("Hailee Steinfeld").onclick = () => selectImage("Hailee Steinfeld");
 document.getElementById("Gal Gadot").onclick = () => selectImage("Gal Gadot");
 document.getElementById("Dua Lipa").onclick = () => selectImage("Dua Lipa");
+//5 Points
 document.getElementById("Olivia Rodrigo").onclick = () => selectImage("Olivia Rodrigo");
-//4$
 document.getElementById("Selena Gomez").onclick = () => selectImage("Selena Gomez");
 document.getElementById("Selma Hayek").onclick = () => selectImage("Selma Hayek");
 document.getElementById("Vanessa Hudgens").onclick = () => selectImage("Vanessa Hudgens");
+//4 Points
 document.getElementById("Nathalie Emmanuel").onclick = () => selectImage("Nathalie Emmanuel");
 document.getElementById("Olivia Munn").onclick = () => selectImage("Olivia Munn");
-//3$
 document.getElementById("Zoe Kravitz").onclick = () => selectImage("Zoe Kravitz");
 document.getElementById("Megan Fox").onclick = () => selectImage("Megan Fox");
+//3 Points
 document.getElementById("Jamie Alexander").onclick = () => selectImage("Jamie Alexander");
 document.getElementById("Kat Dennings").onclick = () => selectImage("Kat Dennings");
 document.getElementById("Priyanka Chopra").onclick = () => selectImage("Priyanka Chopra");
-//2$
 document.getElementById("Kylie Jenner").onclick = () => selectImage("Kylie Jenner");
+//2 Points
 document.getElementById("Naomi Scott").onclick = () => selectImage("Naomi Scott");
 document.getElementById("Tessa Thompson").onclick = () => selectImage("Tessa Thompson");
 document.getElementById("Kendall Jenner").onclick = () => selectImage("Kendall Jenner");
 document.getElementById("Diane Guerrero").onclick = () => selectImage("Diane Guerrero");
-//1$
+//1 Point
 document.getElementById("Ariel Winter").onclick = () => selectImage("Ariel Winter");
 document.getElementById("Emmanuelle Chriqui").onclick = () => selectImage("Emmanuelle Chriqui");
 document.getElementById("Morena Baccarin").onclick = () => selectImage("Morena Baccarin");
