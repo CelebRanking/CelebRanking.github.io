f4c9b2c47b8baf3f2b1b0e5ff4e64ef18f63704e
CelebRanking <privat.acc.for.insta@gmail.com>
2023-07-11 19:50:44 +0200

    new message on hub

--- interactive/gamehub.js
+++ interactive/gamehub.js
@@ -1,9 +1,19 @@
-// Function to show the news pop-up
-function showNewsPopup() {
-  var newsPopup = document.getElementById("news-popup");
-  newsPopup.style.display = "block";
+// Function to show the pop-up
+function showPopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "block";
+}
+
+// Function to hide the pop-up
+function hidePopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "none";
 }
 
 // Add click event listener to the image
 var image = document.getElementById("gamehub-image");
-image.addEventListener("click", showNewsPopup);
+image.addEventListener("click", showPopup);
+
+// Add click event listener to the close button
+var closeButton = document.getElementById("close-button");
+closeButton.addEventListener("click", hidePopup);
